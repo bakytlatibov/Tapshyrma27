847cceb00cfa2e1a3a38b5c46248c99b33b1027f
bakytlatibov <bakytlatibov3@gmail.com>
2023-02-27 17:49:19 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import java.util.function.UnaryOperator;
 
 public class Main {
     public static void main(String[] args) {
+
+        UnaryOperator<Double>s=x->x*x;
+        System.out.println(s.apply(5.0));
+
+
+
+
+
+
         System.out.println(sqrt().apply(36.0));
 
     }
